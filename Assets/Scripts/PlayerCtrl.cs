7b0968c34f829dbdb475d3b13b32284dd0626fdc
Anna Clara Pereira <annaclarapereiradovale@gmail.com>
2018-06-25 11:15:51 -0300

    Fazendo flip horizontal do player

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -8,10 +8,12 @@ public class PlayerCtrl : MonoBehaviour {
 	public float jumpSpeed = 600f;
 
 	Rigidbody2D rb;
-	
+	SpriteRenderer sr; 
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		sr = GetComponent<SpriteRenderer>(); 
 	}
 	
 	// Update is called once per frame
@@ -32,6 +34,12 @@ public class PlayerCtrl : MonoBehaviour {
 
 	void MoveHorizontal(float speed) {
 		rb.velocity = new Vector2(speed, rb.velocity.y);
+		if (speed < 0f) {
+			sr.flipX = true;
+		}
+		else if (speed > 0f) {
+			sr.flipX = false;
+		}
 	} 
 
 	void StopMovingHorizontal() {
