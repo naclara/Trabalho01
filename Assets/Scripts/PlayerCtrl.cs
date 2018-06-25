fd01a27e706799b851fb92d94f70e5b28703be57
Anna Clara Pereira <annaclarapereiradovale@gmail.com>
2018-06-25 11:08:04 -0300

    Fazendo player pular

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour {
 
 	public float horizontalSpeed = 10f; 
+	public float jumpSpeed = 600f;
 
 	Rigidbody2D rb;
 	
@@ -23,6 +24,10 @@ public class PlayerCtrl : MonoBehaviour {
 		else {
 			StopMovingHorizontal();
 		}
+
+		if (Input.GetButtonDown("Jump")) {
+			Jump();
+		}
 	}
 
 	void MoveHorizontal(float speed) {
@@ -31,5 +36,9 @@ public class PlayerCtrl : MonoBehaviour {
 
 	void StopMovingHorizontal() {
 		rb.velocity = new Vector2(0f, rb.velocity.y);
+	}
+
+	void Jump() {
+		rb.AddForce (new Vector2(0f, jumpSpeed));
 	}
 }
