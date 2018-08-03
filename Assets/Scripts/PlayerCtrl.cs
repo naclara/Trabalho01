6c3a49b61afccd834ae7a84b5306da24f134846e
Anna Clara Pereira <annaclarapereiradovale@gmail.com>
2018-08-03 10:12:41 -0300

    Double jump

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -13,6 +13,21 @@ public class PlayerCtrl : MonoBehaviour {
 
 	bool isJumping = false; 
 
+
+	public Transform feet; 
+
+	public float feetWidth = 0.5f;
+
+	public float feetHeight = 0.1f;
+
+	public bool isGrounded;
+
+	public LayerMask whatIsGround; 
+
+	bool canDoubleJump = false;
+
+	public float delayForDoubleJump = 0.2f;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
@@ -20,8 +35,15 @@ public class PlayerCtrl : MonoBehaviour {
 		anim = GetComponent<Animator>();
 	}
 	
+	void OnDrawGizmos() {
+			Gizmos.DrawWireCube(feet.position, new Vector3(feetWidth, feetHeight, 0f));
+	}
+
 	// Update is called once per frame
 	void Update () {
+
+		isGrounded = Physics2D.OverlapBox(new Vector2(feet.position.x, feet.position.y),new Vector2(feetWidth, feetHeight), 360.0f, whatIsGround);
+
 		float horizontalInput = Input.GetAxisRaw ("Horizontal"); // -1: esquerda, 1: direita
 		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput; 
 		if (horizontalPlayerSpeed != 0) {
@@ -65,9 +87,25 @@ public class PlayerCtrl : MonoBehaviour {
 	 }
 
 	void Jump() {
+		if (isGrounded) {
 		isJumping = true;
 		rb.AddForce (new Vector2(0f, jumpSpeed));
 		anim.SetInteger("State", 1);
+
+		Invoke("EnableDoubleJump", delayForDoubleJump);
+	}
+
+		if (canDoubleJump && !isGrounded) {
+		rb.velocity = Vector2.zero;
+		rb.AddForce (new Vector2(0f, jumpSpeed));
+		anim.SetInteger("State", 1);
+		canDoubleJump = false;
+
+		}
+	}
+
+	void EnableDoubleJump() {
+		canDoubleJump = true;
 	}
 
 	void OnCollisionEnter2D(Collision2D other) {
