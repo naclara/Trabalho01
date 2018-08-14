00606146477ef01b1809e0b21d0379eacdcdd9e0
Anna Clara Pereira <annaclarapereiradovale@gmail.com>
2018-08-14 10:44:17 -0300

    Preparando o sistema de particulas das moedas

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -120,6 +120,7 @@ public class PlayerCtrl : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.CompareTag("Coin")) {
+			SFXManager.instance.ShowCoinParticles(other.gameObject);
 			Destroy(other.gameObject);
 		}
 	}
